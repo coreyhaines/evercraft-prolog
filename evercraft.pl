36791e4735094a18baf52db93d4bd99a38bf9a3a
Corey Haines <coreyhaines@gmail.com>
2016-01-06 10:59:36 -0500

    alignment, attack, damage, newHitPoints

---
+++ evercraft.pl
@@ -0,0 +1,25 @@
+%% We have alignments
+alignment(good).
+alignment(neutral).
+alignment(evil).
+
+%% Can attack
+attack(Roll, AC, hit) :-
+  Roll >= AC.
+attack(Roll, AC, miss) :-
+  Roll < AC.
+
+%% Can get damage
+damage(Roll, 1) :-
+  Roll < 20.
+damage(20, 2).
+
+%% Can adjust hit points
+newHitPoints(HP, Damage, 0) :-
+  HP =< Damage.
+newHitPoints(HP, Damage, NHP) :-
+  HP > Damage,
+  NHP is HP - Damage.
+
+%% Can create a character with an alignment
+character('corey', good).
