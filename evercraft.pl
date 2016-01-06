888826b65f9ff2e9fcdea338aff1f0e20324687d
Corey Haines <coreyhaines@gmail.com>
2016-01-06 13:25:02 -0500

    Change attackCharacter to have Attacker and Defender to best calculate damage

--- evercraft.pl
+++ evercraft.pl
@@ -22,21 +22,26 @@ newHitPoints(HP, Damage, NHP) :-
   HP > Damage,
   NHP is HP - Damage.
 
-attackCharacter(Name, Roll) :-
-  character(Name, Aligment, AC, HP),
-  attack(Roll, AC, AttackResult),
+attackCharacter(AttackerName, DefenderName, Roll) :-
+  character(AttackerName, _, _, _),
+  character(DefenderName, DefenderAlignment, DefenderAC, DefenderHP),
+  attack(Roll, DefenderAC, AttackResult),
   damage(AttackResult, Roll, Damage),
-  newHitPoints(HP, Damage, NHP), !,
-  asserta(character(Name, Alignment, AC, NHP)).
+  newHitPoints(DefenderHP, Damage, NHP), !,
+  asserta(character(DefenderName, DefenderAlignment, DefenderAC, NHP)).
 
   %% Trying to figure out how to update the character
 :- dynamic character/4.
 :- dynamic abilities/7.
 
 %% asserta(character('corey', good, 10, 5)).
-%% asserta(abilities('corey', 10, 15, 7, 10, 14, 6)).
+%% asserta(character('nate', evil, 10, 5)).
+%% asserta(abilities('corey', 5, 12, 6, 10, 4, 3)).
+%% asserta(abilities('nate', 6, 6, 16, 20, 6, 9)).
 %%
-%% Roll = 10, character('corey', Alignment, AC, HP), attack(Roll, AC, AttackResult), damage(AttackResult, Roll, Damage), newHitPoints(HP, Damage, NHP), !, asserta(character('corey', Alignment, AC, NHP)).
-%%
+%% attackCharacter('corey', 'nate', 10).
+
 %% character('corey', A, AC, HP), !.
+%% character('nate', A, AC, HP), !.
 %% abilities('corey', Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma), !.
+%% abilities('nate', Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma), !.
