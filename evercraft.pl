4adbd9113dff389e9be3a80bac47898cec3cf78b
Corey Haines <coreyhaines@gmail.com>
2016-01-06 13:18:03 -0500

    Add abilities

--- evercraft.pl
+++ evercraft.pl
@@ -31,9 +31,12 @@ attackCharacter(Name, Roll) :-
 
   %% Trying to figure out how to update the character
 :- dynamic character/4.
+:- dynamic abilities/7.
 
 %% asserta(character('corey', good, 10, 5)).
+%% asserta(abilities('corey', 10, 15, 7, 10, 14, 6)).
 %%
 %% Roll = 10, character('corey', Alignment, AC, HP), attack(Roll, AC, AttackResult), damage(AttackResult, Roll, Damage), newHitPoints(HP, Damage, NHP), !, asserta(character('corey', Alignment, AC, NHP)).
 %%
 %% character('corey', A, AC, HP), !.
+%% abilities('corey', Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma), !.
