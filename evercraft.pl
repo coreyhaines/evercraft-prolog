26389f016b4ef6134df5c56beac5dff293e36383
Corey Haines <coreyhaines@gmail.com>
2016-02-10 08:14:13 -0600

    switch evercraft to calculating hitpoints based on damage incurred

--- evercraft.pl
+++ evercraft.pl
@@ -23,6 +23,28 @@ attack(Roll, AC, hit) :-
 attack(Roll, AC, miss) :-
   Roll < AC.
 
+sum(List, Sum) :-
+  sumRec(List, 0, Sum).
+sumRec([], Value, Value).
+sumRec([H|T], Value, Accum) :-
+  sumRec(T, Value, OldAccum),
+  Accum is OldAccum + H.
+
+%% Add up all the damages that have been done
+damageSoFar(Name, Damage) :-
+  findall(DamagePoint, damageIncurred(Name, DamagePoint), Damages),
+  sum(Damages, Damage).
+
+%% Current hitpoints is starting hit points minus damage done so far, bounded at one
+currentHitPoints(Name, CurrentHP) :-
+  abilities(Name, _, _, Constitution, _, _, _),
+  defaultHP(BaseHP),
+  adjustedHitPoint(Constitution, BaseHP, CharacterHP),
+  damageSoFar(Name, Damage),
+  RawHP is CharacterHP - Damage,
+  boundAtOne(RawHP, CurrentHP).
+
+
 %% Can get damage
 damage(miss, _, _, 0).
 damage(hit, Roll, Strength, Damage) :-
@@ -71,7 +93,7 @@ newExperience(Name, hit, NewXP) :-
 relevantCharacterAttributes(AttackerName, AttackerStrength, DefenderName, DefenderDexterity, DefenderAC, DefenderHP) :-
   abilities(DefenderName, _, DefenderDexterity, _, _, _, _),
   character(DefenderName, _, DefenderAC),
-  hitpoints(DefenderName, DefenderHP),
+  currentHitPoints(DefenderName, DefenderHP),
   abilities(AttackerName, AttackerStrength, _, _, _, _, _), !.
 
 modifiedAttackNumbers(Roll, AttackerName, DefenderName, ModifiedAC, ModifiedRoll) :-
@@ -81,14 +103,18 @@ modifiedAttackNumbers(Roll, AttackerName, DefenderName, ModifiedAC, ModifiedRoll
   ModifiedRoll is Roll + RollModifier,
   ModifiedAC is DefenderAC + ACModifier.
 
+registerDamage(_, miss, _) :-
+  format("Missed~n").
+registerDamage(Name, hit, Damage) :-
+  format("Hit. ~p incurred ~p damage~n", [Name, Damage]),
+  asserta(damageIncurred(Name, Damage)).
+
 attackCharacter(AttackerName, DefenderName, Roll) :-
-  relevantCharacterAttributes(AttackerName, AttackerStrength, DefenderName, _, _, DefenderHP),
+  relevantCharacterAttributes(AttackerName, AttackerStrength, DefenderName, _, _, _),
   modifiedAttackNumbers(Roll, AttackerName, DefenderName, ModifiedAC, ModifiedRoll),
   attack(ModifiedRoll, ModifiedAC, AttackResult),
   damage(AttackResult, Roll, AttackerStrength, Damage),
-  format("~p Damage ~p~n", [AttackResult, Damage]),
-  newHitPoints(DefenderHP, Damage, NHP),
-  asserta(hitpoints(DefenderName, NHP)),
+  registerDamage(DefenderName, AttackResult, Damage),
   newExperience(AttackerName, AttackResult, NewXP),
   asserta(experience(AttackerName, NewXP)),
   showCharacter(AttackerName),
@@ -97,8 +123,8 @@ attackCharacter(AttackerName, DefenderName, Roll) :-
   %% Trying to figure out how to update the character
 :- dynamic character/3.
 :- dynamic abilities/7.
-:- dynamic hitpoints/2.
 :- dynamic experience/2.
+:- dynamic damageIncurred/2.
 
 can(Roll) :-
   attackCharacter('corey', 'nate', Roll).
@@ -107,33 +133,27 @@ nac(Roll) :-
   attackCharacter('nate', 'corey', Roll).
 
 showCharacter(Name) :-
-  character(Name, A, AC),
-  abilities(Name, Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma),
-  hitpoints(Name, HP),
+  character(Name, _A, _AC),
+  abilities(Name, _Strength, _Dexterity, _Constitution, _Wisdom, _Intelligence, _Charisma),
+  currentHitPoints(Name, HP),
   characterLevel(Name, Level),
   experience(Name, XP), !,
   format("~p HP: ~p XP: ~p Level: ~p~n", [Name, HP, XP, Level]).
 
 
-%% defaultHP(DefaultHP),
 %% defaultAC(DefaultAC),
 %% defaultXP(DefaultXP),
 %% asserta(abilities('corey', 5, 12, 6, 10, 4, 3)),
 %% abilities('corey', Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma), !,
-%% adjustedHitPoint(Constitution, DefaultHP, AdjustedHP),
 %% asserta(character('corey', good, DefaultAC)),
-%% asserta(hitpoints('corey', AdjustedHP)),
 %% asserta(experience('corey', DefaultXP)),
 %% character('corey', A, AC), !.
 
-%% defaultHP(DefaultHP),
 %% defaultAC(DefaultAC),
 %% defaultXP(DefaultXP),
 %% asserta(abilities('nate', 6, 6, 16, 20, 6, 9)),
 %% abilities('nate', Strength, Dexterity, Constitution, Wisdom, Intelligence, Charisma), !,
-%% adjustedHitPoint(Constitution, DefaultHP, AdjustedHP),
 %% asserta(character('nate', good, DefaultAC)),
-%% asserta(hitpoints('nate', AdjustedHP)),
 %% asserta(experience('nate', DefaultXP)),
 %% character('nate', A, AC), !.
 %%
